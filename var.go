7b299c5fd96c5b9feaf6918c82ac2b1ea5d8a818
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-09-12 22:50:59 +0600

    learned about variables and data types

---
+++ var.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func printVar() {
+	
+	// a:=10 
+	var a = 10
+
+	// a = 10  - if we want to reassign but it has to be the same type
+
+	var b int = 20 
+
+	const p = 100 // we can't change value of const
+
+	fmt.Println(a,b,p)
+}
+
+/*
+data types are 
+string, 
+bool, 
+int, int8, int16, int32, int64, 
+uint, uint8, uint16, uint32, uint64, 
+float32, float64
+*/
+
