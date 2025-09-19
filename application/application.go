71a30f6da87264c60e5652e9cc3c6eaaec78eb5c
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-09-19 23:54:42 +0600

    learned about structuring

--- application/application.go
+++ application/application.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
-func application() {
-	//welcome message 
-	fmt.Println("Welcome to the application!")
 
-	//Taking ser input 
-	var name string 
+func printWelcome() {
+	//welcome message
+	fmt.Println("Welcome to the application!")
+}
+
+func nameInput() string {
+	//Taking user input and returning the value
+	var name string
 	fmt.Println("Enter your name -")
 	fmt.Scanln(&name)
 
-	var num1 int 
-	var num2 int 
+	return name
+}
+
+func numsInput() int {
+	//taking nums input and returning the value
+	var num1 int
+	var num2 int
 	fmt.Println("Enter first number -")
 	fmt.Scanln(&num1)
 	fmt.Println("Enter second number -")
@@ -20,17 +28,35 @@ func application() {
 
 	sum := num1 + num2
 
-	// Displaying results 
+	return sum
+}
+
+func display(name string, sum int) {
+	// Displaying results
 	fmt.Println("Hello, ", name)
 	fmt.Println("The sum of your numbers is : ", sum)
 
-	// Goodbye message 
+	// Goodbye message
 	fmt.Println("Thank you for using the application, ", name)
 	fmt.Println("Goodbye!")
+}
 
+
+func application() {
+	// storing all the information
+
+	printWelcome()
+
+	name := nameInput()
+
+	sum := numsInput()
+
+	display(name, sum)
 
 }
 
+
 func main() {
+	//calling the whole application at once
 	application()
 }
