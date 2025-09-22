b8f10d2c7b9937525b9c79dc4a484531274acb6c
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-09-22 23:48:39 +0600

    learned about scope and how ram works

--- scope/scope.go
+++ scope/scope.go
@@ -8,6 +8,8 @@ var b = 30
 
 func add(x int, y int){
 	z := x + y 
+	// z := x + a  a could be accessed here as it is global 
+	// z := x + q  q couldn't be accessed as it is in block scope 
 	fmt.Println(z)
 }
 
