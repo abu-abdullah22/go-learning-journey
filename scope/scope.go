db4be6a1f7c7f41b1ff2dd3e8745e91465d31c09
abu-abdullah22 <rorschachmayhem@gmail.com>
2026-08-02 15:15:38 +0600

    Refactor code formatting and improve readability in scope.go

--- scope/scope.go
+++ scope/scope.go
@@ -2,27 +2,29 @@ package main
 
 import "fmt"
 
+// Global scope
 var a = 20
-var b = 30 
+var b = 30
 
-
-func add(x int, y int){
-	z := x + y 
-	// z := x + a  a could be accessed here as it is global 
-	// z := x + q  q couldn't be accessed as it is in block scope 
+func add(x int, y int) {
+	// Block scope
+	z := x + y
+	// z := x + a  a could be accessed here as it is global
+	// z := x + q  q couldn't be accessed as it is in block scope
 	fmt.Println(z)
 }
 
+func main() {
 
-func main(){
+	// Block scope
 	var p = 30
-	var q = 40 
+	var q = 40
 
-	add(p,q)
+	add(p, q)
 
-	add(a,b )
+	add(a, b)
 
-	add(a,p)
+	add(a, p)
 
-	// add(b, z) --- z can't be accessed here 
+	// add(b, z) --- z can't be accessed here
 }
