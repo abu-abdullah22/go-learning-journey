0efbead4420387c0b8fe02deab3768b3d25a2a94
abu-abdullah22 <rorschachmayhem@gmail.com>
2026-08-04 19:43:12 +0600

    Enhance printNum function to handle positive and non-positive numbers

--- Scope_again/main.go
+++ Scope_again/main.go
@@ -2,20 +2,34 @@ package main
 
 import "fmt"
 
+// global scope
 var (
 	a = 10
 	b = 20
 )
 
+// global scope
 func printNum(num int) {
-	fmt.Println(num)
+	// local scope
+	if num > 0 {
+		// block scope
+		j := 100
+		fmt.Println(num + j)
+	} else {
+		fmt.Println("Number is not positive")
+	}
+
 }
 
+// global scope
 func add(x int, y int) {
-	res := x + y 
+	// local scope
+	res := x + y
 	printNum(res)
 }
 
+// global scope
 func main() {
-	add(a,b)
+	// local scope
+	add(a, b) // a, b are global variables
 }
